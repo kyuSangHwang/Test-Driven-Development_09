849a7999806918f362a3bf4127836e6291fe5f19
KS <kyusang.dev@gmail.com>
2024-07-11 14:36:14 +0900

    Sync

--- Albertos/AlbertosTests/MenuListViewModelTests.swift
+++ Albertos/AlbertosTests/MenuListViewModelTests.swift
@@ -11,31 +11,31 @@ import Combine
 @testable import Albertos
 
 final class MenuListViewModelTests: XCTestCase {
-
+    
     var cancellables = Set<AnyCancellable>()
-
+    
     func testWhenFetchingStartsPublishesEmptyMenu() {
         let viewModel = MenuList.ViewModel()
         XCTAssertTrue(viewModel.sections.isEmpty)
     }
-
+    
     func testWhenSucceedsFetchingSectionsReceivedMenuAndGivenGroupingClosure() {
         var receivedMenu: [MenuItem]?
         let expectedSections: [MenuSection] = [.fixture()]
-
+        
         let spyClosure: ([MenuItem]) -> [MenuSection] = { items in
             receivedMenu = items
             return expectedSections
         }
-
+        
         let viewModel = MenuList.ViewModel(
             menuFetching: MenuFetchingSample(),
             menuGrouping: spyClosure
         )
-
+        
         let expectation = XCTestExpectation(
             description: "받은 메뉴와 주어진 그룹화 클로저를 사용하여 생성된 섹션들을 발행합니다.")
-
+        
         viewModel.$sections
             .dropFirst()
             .sink { value in
@@ -44,6 +44,7 @@ final class MenuListViewModelTests: XCTestCase {
                 expectation.fulfill()
             }
             .store(in: &cancellables)
-
+        
         wait(for: [expectation], timeout: 1)
     }
+}
